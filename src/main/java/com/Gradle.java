55f1a4ee6fa5a0165031794cc901e21eabb06d88
Andrzej <@>
2018-03-14 16:14:41 +0100

    Bug fixes

--- src/main/java/com/Gradle.java
+++ src/main/java/com/Gradle.java
@@ -1,7 +1,10 @@
 package com;
 
+import org.apache.commons.lang3.event.EventUtils;
+
 public class Gradle {
     public static void main(String[] args) {
+        EventUtils eventUtils = new EventUtils();
         System.out.println("Hello Gradle!");
     }
 }
